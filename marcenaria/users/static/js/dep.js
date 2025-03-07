298d8585270632ffa28ed7d0c839a2b61242156f
Gabriel Galdino <gabrielcgaldiino@gmail.com>
2025-03-07 17:19:30 -0300

    Atualização na NAVBAR, exibição de nome do usuário.

--- marcenaria/users/static/js/dep.js
+++
@@ -1,6 +0,0 @@
-user = document.getElementById('id-username').getAttribute('value')
-pass = document.getElementById('id-password').getAttribute('value')
-
-document.querySelector('.btn').addEventListener('submit', function() {
-    console.log(user, pass)
-})
